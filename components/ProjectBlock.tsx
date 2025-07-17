a36304ba3dc9b29ce46bbbc287fe77c4fea22a2d
Nathan <yeenathan21@gmail.com>
2025-07-17 16:38:20 -0700

    work page styling and content

--- components/ProjectBlock.tsx
+++ components/ProjectBlock.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 
-export default function ProjectBlock({name, copy, image, url, wide=false}) {
-  const mainStyle = !wide?
-    "flex flex-col justify-center items-center w-full"
-    :
-    "flex flex-col justify-center items-start w-full md:col-span-2"
+export default function ProjectBlock({name, copy, image, url, wide=false}:{name:string, copy:string, image:string, url:string, wide?:boolean}) {
+  
   return(
-    <div className={mainStyle}>
+    <div className={`flex flex-col justify-center w-full ${wide?"items-start md:col-span-2":"items-center"}`}>
       <img src={image} className="rounded-xl"/>
-      <div className="flex flex-col items-start justify-start gap-3">
+      <div className={`flex flex-col items-start justify-start gap-3 ${wide?"absolute top-145 pl-12 text-white":undefined}`}>
         <h3 className="text-3xl font-['Roobert'] font-medium mt-3">{name}</h3>
-        <p className="text-l text-gray-700 md:max-w-1/2">{copy}</p>
+        <p className={`text-l md:max-w-1/2 ${wide?"text-white":"text-gray-700"}`}>{copy}</p>
         <Link className="italic underline text-xl mt-2 hover:text-gray-500" href={url}>VIEW PROJECT</Link>
       </div>
     </div>
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -6,10 +6,10 @@ export default function Work() {
       <h1 className="text-center justify-center text-8xl font-medium font-['Roobert'] mb-10">
         Our Work
       </h1>
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-4">
-        <ProjectBlock name={"Harmony"} url={"/work/harmony"} wide={true} image={"/images/harmony/home-card.jpg"} copy={"An app designed to support caregivers by simplifying tasks, building connections, and empowering families with meaningful tools."}/>
-        <ProjectBlock name={"Circles"} url={"/work/circles"} image={"/images/circles/home-card.png"} copy={"An app designed to simplify sharing with friends by making it more private, meaningful, and true to real life."}/>
-        <ProjectBlock name={"Mélange"} url={"/work/melange"} image={"/images/melange/home-card.png"} copy={"A website specializing in contemporary art prints and posters by emerging global artists."}/>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-start">
+        <ProjectBlock name={"Harmony"} url={"/work/harmony"} wide={true} image={"/images/work/harmony-bg.png"} copy={"An app designed to support caregivers by simplifying tasks, building connections, and empowering families with meaningful tools."}/>
+        <ProjectBlock name={"Circles"} url={"/work/circles"} image={"/images/work/circles-mockup.png"} copy={"An app designed to simplify sharing with friends by making it more private, meaningful, and true to real life."}/>
+        <ProjectBlock name={"Mélange"} url={"/work/melange"} image={"/images/work/melange-mockup.png"} copy={"A website specializing in contemporary art prints and posters by emerging global artists."}/>
       </section>
     </main>
   )
