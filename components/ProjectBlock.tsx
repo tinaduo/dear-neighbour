6d0a4b5b3d1d78fd1c3e41b7b522aebb61b91671
yeenathan <yeenathan21@gmail.com>
2025-07-18 16:10:32 -0700

    fix contact page

--- components/ProjectBlock.tsx
+++ components/ProjectBlock.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function ProjectBlock({name, copy, image, url, wide=false}:{name:string, copy:string, image:string, url:string, wide?:boolean}) {
   
   return(
-    <div className={`flex flex-col justify-center w-full ${wide?"items-start md:col-span-2":"items-center"}`}>
+    <div className={`flex flex-col justify-center w-full ${wide?"items-start md:col-span-2 md:relative":"items-center"}`}>
       <img src={image} className="rounded-xl"/>
-      <div className={`flex flex-col items-start justify-start gap-3 ${wide?"absolute top-145 pl-12 text-white":undefined}`}>
+      <div className={`flex flex-col items-start justify-start gap-3 ${wide?"md:absolute bottom-12 md:pl-12 md:text-white":undefined}`}>
         <h3 className="text-3xl font-['Roobert'] font-medium mt-3">{name}</h3>
-        <p className={`text-l md:max-w-1/2 ${wide?"text-white":"text-gray-700"}`}>{copy}</p>
+        <p className={`text-l md:max-w-1/2 ${wide?"md:text-white":"text-gray-700"}`}>{copy}</p>
         <Link className="italic underline text-xl mt-2 hover:text-gray-500" href={url}>VIEW PROJECT</Link>
       </div>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,52 +1,45 @@
-import Footer from "../../../components/Footer";
-import NavBar from "../../../components/NavBar";
-
 export default function Services() {
     return (
-        <>
-            <NavBar />
-            <main className="flex flex-col mx-4 items-center justify-center min-h-screen space-y-40 my-16 2xl:mx-[195px]">
-                <section className="flex flex-col items-center">
-                    <h1 className="text-center justify-center text-8xl font-medium font-['Roobert']">
-                        Let’s Work Together!
-                    </h1>
-                    <p className="text-center text-xl font-['FavoritMono'] mt-10 mb-8 max-w-8/12 tracking-tighter text-gray">
-                        Have a question, a thought, or something you'd like to share? Say hello and we’ll take it from there!
-                    </p>
+        <main>
+            <section className="flex flex-col items-center">
+                <h1 className="text-center justify-center text-8xl font-medium font-['Roobert']">
+                    Let’s Work Together!
+                </h1>
+                <p className="text-center text-xl font-['FavoritMono'] mt-10 mb-8 max-w-8/12 tracking-tighter text-gray">
+                    Have a question, a thought, or something you'd like to share? Say hello and we’ll take it from there!
+                </p>
+            </section>
+            <section className="flex flex-col items-center relative">
+                <img src="/images/postcards/sticky-note.png" alt="Sticky Note" className=" " />
+                <div className="flex flex-col items-center justify-center space-y-4 absolute bottom-0 top-0 pl-20">
+                    <div>
+                    <h6
+                        className="opacity-80 text-center justify-center text-stone-900/75 text-2xl font-['FavoritMono']"
+                    >
+                        EMAIL                         
+                    </h6>
+                        <a
+                            href="mailto:dear.neighbour@outlook.ca"
+                            className="text-darkgrey hover:underline text-4xl font-['Roobert'] text-center"
+                        >
+                            dear.neighbour@outlook.ca
+                        </a>
+                    </div>
+                    <div className="flex flex-col items-center mt-8">
+                        <h6
+                            className="opacity-80 text-center justify-center text-stone-900/75 text-2xl font-['FavoritMono']"
+                        >
+                            INSTAGRAM                         
+                        </h6>
+                        <a
+                            href="https://www.instagram.com/@_dearneighbour/"
+                            className="text-darkgrey hover:underline text-4xl font-['Roobert'] text-center"
+                        >
+                            @_dearneighbour
+                        </a>
+                    </div>
+                </div>
                 </section>
-                <section className="flex flex-col items-center space-y-8">
-                  <img src="/images/postcards/sticky-note.png" alt="Sticky Note" className=" " />
-                      <div className="flex flex-col items-center justify-center space-y-4 absolute top-195 pl-20">
-                          <div>
-                            <h6
-                                className="opacity-80 text-center justify-center text-stone-900/75 text-2xl font-['FavoritMono']"
-                            >
-                                EMAIL                         
-                            </h6>
-                                <a
-                                    href="mailto:dear.neighbour@outlook.ca"
-                                    className="text-darkgrey hover:underline text-4xl font-['Roobert'] text-center"
-                                >
-                                    dear.neighbour@outlook.ca
-                                </a>
-                          </div>
-                          <div className="flex flex-col items-center mt-8">
-                              <h6
-                                  className="opacity-80 text-center justify-center text-stone-900/75 text-2xl font-['FavoritMono']"
-                              >
-                                  INSTAGRAM                         
-                              </h6>
-                                <a
-                                    href="https://www.instagram.com/@_dearneighbour/"
-                                    className="text-darkgrey hover:underline text-4xl font-['Roobert'] text-center"
-                                >
-                                    @_dearneighbour
-                                </a>
-                            </div>
-                        </div>
-                </section>
-            </main>
-            <Footer />
-        </>
+        </main>
     );
 }
